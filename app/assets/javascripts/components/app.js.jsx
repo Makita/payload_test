c9ee53a7acafb29b283ea7f51d165e3d569d86b1
Madobe <sin_emerald@hotmail.com>
2018-10-14 08:52:39 -0600

    Realized since it's a single page for real now, there's no need to remove prerendering

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -3,7 +3,6 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      currentPage: window.location.hash || "#list",
       tickets: props.tickets,
       events: props.events,
     };
